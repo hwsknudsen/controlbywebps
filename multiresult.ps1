39d00134e43991728bfc6fb4e7135a5b364b64cc
hwsknudsen <hwsknudsen@users.noreply.github.com>
2024-06-05 10:44:24 -0300

    Update multiresult.ps1

--- multiresult.ps1
+++ multiresult.ps1
@@ -1,25 +1,31 @@
 $username = "user"
-$password = "pw"
+$password = "a"
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username,$password)))
 
 $response = Invoke-RestMethod -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Uri "http://url/customState.json?showUnits=0&showColors=0"
-$int = [Math]::Round($response.'dieselVolume(gallons)',0)
+$x = [string][Math]::Round($response.'dieselVolume(gallons)',0)
+$y = [string][Math]::Round($response.'waterVolume(gallons)',0)
 
-$int2 = [Math]::Round($response.'waterVolume(gallons)',0)
-
-$x=[string]$int+":OK"
-$y=[string]$int
-$z=[string]$int2
+$z = [string][Math]::Round($response.'dieselLevel(mm)',0)
+$d = [string][Math]::Round($response.'waterLevel(mm)',0)
 
 write-output "
            <prtg>
            <result>
            <channel>dieselVolume(gallons)</channel>
-           <value>$y</value>
+           <value>$x</value>
            </result>
            <result>
            <channel>waterVolume(gallons)</channel>
+           <value>$y</value>
+           </result>
+           <result>
+           <channel>dieselLevel(mm)</channel>
            <value>$z</value>
+           </result>
+           <result>
+           <channel>waterLevel(mm)</channel>
+           <value>$d</value>
            </result>
            </prtg>
 "
